b5184cd50cba2d79bcda05c89275b9bd98d10c85
Sravan Kumar <75154116+Etikala7607@users.noreply.github.com>
2022-03-28 19:41:36 -0500

    users accesses

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  times=times+1;
+  res.send('User accesses are: '+times);
 });
 
 module.exports = router;
